e3bff21fcc87494b4d968a6a6bb9e9de5ff23423
nhatlan <vanphamnhatlan93@gmail.com>
2015-07-10 00:04:44 +0700

    layout test 01 finish

--- index.php
+++ index.php
@@ -5,7 +5,10 @@
         <meta name ="viewport" content="width=device=width"/> 
         <title>My Portfolio</title> 
         <link rel="stylesheet" type="text/css" href="style-main.css">
-        <script src=""></script>
+        <?php $foo =[
+            'first' => true,
+            'long_second' => true];
+         ?>
     </head>
     <body> 
         <h1> Become the designer your startup needs. </h1>
@@ -19,28 +22,75 @@
             </ul>
         </section>
         <article>
-            <h3>Learning is simple.</h3>
+            <h2>Learning is simple.</h2>
             <p>Learning design doesn’t have to be insurmountable. Great design i... just lofty theory. Hone your instincts. Train your eye. Employ ba... techniques and see an immediate improvement in your work.
 
 </p>
-            <h3>Design for success.</h3>
+            <h2>Design for success.</h2>
             <p>Design can produce measurable gains for your business. Learn to p... emphasis and guide users to take action. Earn the trust your busi... deserves with a polished, professional design.
 
 </p>
-            <h3>You are a designer.</h3>
+            <h2>You are a designer.</h2>
             <p>You are capable of beautiful design. Learn to start with a blankexperience worthy of your elegant code.
 
 </p>
         </article>
         <section>
-            Why spend $1000s hiring a designer?
-            <h3>
+            <h2>Why spend $1000s hiring a designer?</h2>
+            <p>
                 “I highly recommend it for developers and product builders...I cr... good enough design myself, without much time or money, and was able to launch my product. —Eric Davis, @edavis10
 
-            </h3>
+            </p>
+            <p>
+                “Design is often described as something mysterious, that can only... accomplished by the talented few. That's bullshit. Design is a sk... can be learned. It's also a process that can be reverse-engineer...
+
+degree. Jarrod's book will give you a huge head-start in both. —Jarkko Laine, @jarkko
+            </p>
+            <h2>What's in the book</h2>
+            <ul>
+                <li>155 p PDF</li>
+                <li>epub</li>
+                <li>mobi</li>
+                <li>2 printable</li>
+                <li>cheatsheet</li>
+            </ul>
         </section>
-        
+        <section>
+            <h3>Revised Edition</h3>
+            <p>Includes new content and revisions requested by readers. Buy Now or Download a Sample</p>
+        </section>   
+        <section>
+            <h4>FAQ section</h4>
+            <p>Why should you become the designer?
+Because designers are expensive and templates are created without any understanding of the problem they propose to solve. With basic
+
+knowledge, you can create a design that is more than good enough to launch a new business.
+
+Who is this book for?
+The book is written primarily for programmers, but founders of al... backgrounds and skill sets will learn just as well. Those not fam...
+
+with coding might not recognize a couple of terms, but the core principles are accessible to everyone.
+
+Does the book cover this or that specific design topic?
+This book contains the minimum design fundamentals that bootstrappers must understand in order to launch a business.
+My intent is to emphasize design basics. You’ll notice some topic... even popular ones, are absent. This is not because such topics are unimportant but because they are neither suitable for beginne...
+
+nor relevant to their bottom line.
+
+Note about .ePub & .Mobi formats:
+For better viewing of the graphics and illustrations, consider us...
+
+the PDF version of this eBook. The .ePub and .Mobi formats provi... limited control over graphics and typography, and thus are less ... for a design book. I've made every effort to ensure they are rea...
+
+and useful for quick reference.</p>
+        </section>     
+        <section>
+            <h4>About </h4>
+            <p>I’m a web designer and bootstrapper. I believe in bootstrappers a...
+
+the businesses they are building, and I know design can help them succeed. I’ve completed work for such diverse clients as funded ... startups, financial companies, movie studios, and consumer brands working at agencies and as a freelancer. I write code. I prefer ... things done to just talking about it. I want to help your startu... with practical but effective design.
+</p>
+        </section>
     </body>
-
-
+    <footer></footer>
 </html>
