f9465eb0886b25d40f4919d9b0e75a1d78dec792
nhatlan <vanphamnhatlan93@gmail.com>
2015-07-09 23:19:21 +0700

    layout test 01

--- index.php
+++ index.php
@@ -4,10 +4,43 @@
         <meta charset ="utf-8"/> 
         <meta name ="viewport" content="width=device=width"/> 
         <title>My Portfolio</title> 
-        <link rel="stylesheet" type="text/css" href="reset.css">
+        <link rel="stylesheet" type="text/css" href="style-main.css">
+        <script src=""></script>
     </head>
-    <body>
+    <body> 
+        <h1> Become the designer your startup needs. </h1>
+        <section>
+            <ul>
+                <li> 155p PDF </li>
+                <li>ePub</li>
+                <li>mobi</li>
+                <li>printable</li>
+                <li>cheatsheet</li>
+            </ul>
+        </section>
+        <article>
+            <h3>Learning is simple.</h3>
+            <p>Learning design doesn’t have to be insurmountable. Great design i... just lofty theory. Hone your instincts. Train your eye. Employ ba... techniques and see an immediate improvement in your work.
 
+</p>
+            <h3>Design for success.</h3>
+            <p>Design can produce measurable gains for your business. Learn to p... emphasis and guide users to take action. Earn the trust your busi... deserves with a polished, professional design.
+
+</p>
+            <h3>You are a designer.</h3>
+            <p>You are capable of beautiful design. Learn to start with a blankexperience worthy of your elegant code.
+
+</p>
+        </article>
+        <section>
+            Why spend $1000s hiring a designer?
+            <h3>
+                “I highly recommend it for developers and product builders...I cr... good enough design myself, without much time or money, and was able to launch my product. —Eric Davis, @edavis10
+
+            </h3>
+        </section>
+        
     </body>
+
 
 </html>
