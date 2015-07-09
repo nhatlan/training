e771c017e5891ece3fc862e587a602419ad7c116
nhatlan <vanphamnhatlan93@gmail.com>
2015-07-09 17:01:16 +0700

    try boilerplate

---
+++ index.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html lang="en">
+    <head>
+        <meta charset ="utf-8"/> 
+        <meta name ="viewport" content="width=device=width"/> 
+        <title>My Portfolio</title> 
+        <link rel="stylesheet" type="text/css" href="reset.css">
+    </head>
+    <body>
+
+    </body>
+
+</html>
